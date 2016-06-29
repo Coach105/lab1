a7e7698b666dc6e2ee07155152a9b979975ccf71
Coach105 <kostya.goncharuk1@gmail.com>
2016-06-29 17:51:37 +0300

    sddsadsdadadsdijjfekjkx

---
+++ src/com/company/Worker.java
@@ -0,0 +1,55 @@
+package com.company;
+
+/**
+ * Created by Kostya on 29.06.2016.
+ * point-to-point messaging
+ */
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Worker {
+// ("non_persistent_queue" - 2nd=false, persistent "task_queue" 2nd=true )
+    private static final String TASK_QUEUE_NAME = "my_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+// 2nd parameter in queueDeclare - message persistence
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false); // Acknowlegment
+                }
+            }
+        };
+
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
